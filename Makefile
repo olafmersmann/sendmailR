a69d8a43be82d947c953c7e2b7ff6cd5b8d16eb2
qsweber <quinnsweber@gmail.com>
2014-09-16 16:51:32 -0400

    Supports multiple recipients
    )
    # Base case:
    sendmail(from = 'from@email.com', to = 'to1@email.com', subject =
    'foo', msg = 'bar')
    
    2)
    # Multiple recipients:
    sendmail(from = 'from@email.com', to =
    c('to1@email.com','to2@email.com'), subject = 'foo', msg = 'bar')
    
    3)
    # Multiple emails as list (equivalent to #2)
    sendmail(from = 'from@email.com', to =
    list('to1@email.com','to2@email.com'), subject = 'foo', msg = 'bar')
    
    4)
    # Multiple emails as named list (to, cc, bcc)
    sendmail(from = 'from@email.com', to =
    list('to'=c('to1@email.com','to2@email.com'),'cc'=c('cc1@email.com','cc2
    @email.com'),'bcc'=c('bcc1@email.com','bcc2@email.com')), subject =
    'foo', msg = 'bar')
    
    5)
    # Multiple emails as named list of lists (equivalent to #4)
    sendmail(from = 'from@email.com', to =
    list('to'=list('to1@email.com','to2@email.com'),'cc'=list('cc1@email.com
    ','cc2@email.com'),'bcc'=list('bcc1@email.com','bcc2@email.com')),
    subject = 'foo', msg = 'bar')
    
    When 'cc' or 'bcc' is in the named list, they will be added to the
    correct email header.

---
+++ Makefile
@@ -0,0 +1,52 @@
+R	= R
+RSCRIPT	= Rscript
+
+.SILENT:
+.PHONEY: clean test check build install package data usage help
+
+usage:
+	echo "Available targets:"
+	echo ""
+	echo " install  - install the package, writing the output into install.log"
+	echo " test     - install package and run unit tests"
+	echo " check    - run R CMD check on the package"
+	echo " help     - shows all available targets"
+
+help: usage
+	echo " clean    - clean up package cruft"
+	echo " package  - build source package of last commit"
+	echo " pkg      - roxygenize skel/ into pkg/"
+	echo " data     - generate R data files from raw text files"
+
+install: clean pkg
+	echo "Installing package..."
+	$(R) CMD INSTALL --no-multiarch pkg > install.log 2>&1
+
+test: install
+	echo "Running unit tests..."
+	$(RSCRIPT) pkg/inst/unittests/runner.r
+
+check: clean pkg
+	echo "Running R CMD check..."
+	$(R) CMD check pkg && rm -fR pkg.Rcheck
+
+clean:
+	echo "Cleaning up..."
+	rm -fR skel/src/*.o skel/src/*.so skel.Rcheck
+	rm -fR pkg
+	rm -fR .RData .Rhistory build.log install.log roxygen.log
+
+package: pkg
+	echo "Building package..."
+	echo "Date: $(date +%Y-%m-%d)" >> pkg/DESCRIPTION
+	git log --no-merges -M --date=iso skel/ > pkg/ChangeLog
+	$(R) CMD build pkg > build.log 2>&1
+	-git stash pop -q
+	rm -f pkg/ChangeLog
+
+pkg: clean
+	echo "Roxygenizing package..."
+	$(RSCRIPT) ./tools/roxygenize > roxygen.log 2>&1
+	rm -fR pkg/inst ## Stupid roxygen!
+	echo "Updating 'Version' field..."
+	$(RSCRIPT) ./tools/set-version
---
+++ skel/R/sendmailR.r
@@ -0,0 +1,192 @@
+##
+## sendmailR.r - send email from within R
+##
+## Author:
+##  Olaf Mersmann (OME) <olafm@datensplitter.net>
+##
+
+.rfc2822_date <- function(time=Sys.time()) {
+  lc <- Sys.getlocale("LC_TIME")
+  on.exit(Sys.setlocale("LC_TIME", lc))
+  Sys.setlocale("LC_TIME", "C")
+  strftime(time, format="%a, %d %b %Y %H:%M:%S -0000",
+           tz="UTC", use.tz=TRUE)
+}
+
+.write_mail <- function(headers, msg, sock) {
+  if (!is.list(msg))
+    msg <- list(msg)
+
+  ## Generate MIME headers:
+  boundary <- paste(packBits(sample(0:1, 256, TRUE)), collapse="")
+  headers$`MIME-Version` <- "1.0"
+  headers$`Content-Type` <- sprintf("multipart/mixed; boundary=\"%s\"", boundary)
+  if ( is.list(headers$To) & !is.null(names(headers$To)) ) { 
+    names(headers$To) <- tolower(names(headers$To))
+    if ( 'bcc' %in% names(headers$To) ) headers$BCC <- paste(unlist(headers$To['bcc']),collapse=', ')
+    if ( 'cc' %in% names(headers$To) ) headers$CC <- paste(unlist(headers$To['cc']),collapse=', ') 
+    if ( 'to' %in% names(headers$To) ) { 
+      headers$To <- paste(unlist(headers$To['to']),collapse=', ')
+    } else { 
+      stop('To must be provided') 
+    }
+  } else {
+    headers$To <- paste(as.character(unlist(headers$To)),collapse=', ')
+  }
+
+  writeLines(paste(names(headers),
+                   unlist(headers), sep=": "),
+             sock, sep="\r\n")
+  writeLines("", sock, sep="\r\n")
+
+  writeLines("This is a message with multiple parts in MIME format.", sock, sep="\r\n")
+
+  for (part in msg) {
+    writeLines(sprintf("--%s", boundary), sock, sep="\r\n")
+    if (inherits(part, "mime_part"))
+      .write_mime_part(part, sock)
+    else if (is.character(part)) { ## Legacy support for plain old string
+      ## writeLines(sprintf("--%s", boundary), sock, sep="\r\n")
+      writeLines("Content-Type: text/plain; format=flowed\r\n", sock, sep="\r\n")
+      writeLines(part, sock, sep="\r\n")
+    }
+  }
+  writeLines(sprintf("--%s--", boundary), sock, sep="\r\n")
+}
+
+.smtp_submit_mail <- function(server, port, headers, msg, verbose=FALSE) {
+  stopifnot(is.character(headers$From))
+  
+  wait_for <- function(lcode) {
+    done <- FALSE
+    while (!done) {
+      line <- readLines(con=sock, n=1)
+      if (verbose)
+        message("<< ", line)
+      code <- substring(line, 1, 3)
+      msg <- substring(line, 5)
+      if (code == lcode) {
+        done <- TRUE
+      } else {
+        if (code >= 500 & code <= 599)
+          stop("SMTP Error: ", msg)
+        else
+          message("Unknown SMTP code: ", code)
+      }
+      
+    }
+    return(list(code=code, msg=msg))
+  }
+
+  send_command <- function(cmd, code) {
+    if (verbose)
+      message(">> ", cmd)
+    writeLines(cmd, sock, sep="\r\n")
+    wait_for(code)
+  }
+
+  nodename <- Sys.info()[4]
+  sock <- socketConnection(host=server,
+                           port=port,
+                           blocking=TRUE)
+  if (!isOpen(sock))
+    stop(sprintf("Could not connect to smtp server '%s' on port '%i'.",
+                 server, port))
+  on.exit(close(sock))
+  ## << 220 <hostname> ESMTP
+  wait_for(220)
+  ## >> HELO localhost
+  ## << 250 mail.statistik.uni-dortmund.de
+  send_command(paste("HELO ", nodename), 250)
+  ## >> MAIL FROM: <foo@bah.com>
+  ## << 250 2.1.0 Ok
+  send_command(paste("MAIL FROM: ", headers$From), 250)
+  ## >> RCPT TO: <bah@baz.org>
+  ## << 250 2.1.5 Ok
+  apply(as.array(as.character(unlist(headers$To))), 1, function(x) { send_command(paste("RCPT TO: ", x), 250) })
+  ## >> DATA
+  ## << 354 blah fu
+  send_command("DATA", 354)
+  ## >> <actual message + headers + .>
+  if (verbose)
+    message(">> <message data>")
+
+  .write_mail(headers, msg, sock)
+  
+  writeLines(".", sock, sep="\r\n")
+  
+  wait_for(250)
+  ## << 250 2.0.0 Ok: queued as XXXXXXXX
+  ## >> QUIT
+  ## << 221 2.0.0 Bye
+  send_command("QUIT", 221)
+}
+
+##' Simplistic sendmail utility for R. Uses SMTP to submit a message
+##' to a local SMTP server.
+##'
+##' @title Send mail from within R
+##'
+##' @param from From whom the mail message is (RFC2822 style address).
+##' @param to Recipient of the message (valid RFC2822 style address).
+##' @param subject Subject line of message.
+##' @param msg Body text of message or a list containing
+##'   \code{\link{mime_part}} objects.
+##' @param \dots ...
+##' @param headers Any other headers to include.
+##' @param control List of SMTP server settings. Valid values are the
+##'   possible options for \code{\link{sendmail_options}}.
+##'
+##' @seealso \code{\link{mime_part}} for a way to add attachments.
+##' @keywords utilities
+##' 
+##' @examples
+##' \dontrun{
+##' from <- sprintf("<sendmailR@@\\%s>", Sys.info()[4])
+##' to <- "<olafm@@datensplitter.net>"
+##' subject <- "Hello from R"
+##' body <- list("It works!", mime_part(iris))
+##' sendmail(from, to, subject, body,
+##'          control=list(smtpServer="ASPMX.L.GOOGLE.COM"))
+##' }
+##'
+##' @export
+sendmail <- function(from, to, subject, msg, ...,
+                     headers=list(),
+                     control=list()) {
+  ## Argument checks:
+  stopifnot(is.list(headers), is.list(control))
+  if (length(from) != 1)
+    stop("'from' must be a single address.")
+    
+  get_value <- function(n, default="") {
+    if (n %in% names(control)) {
+      return(control[[n]])
+    } else if (n %in% names(.SendmailREnv$options)) {
+      return(.SendmailREnv$options[[n]])
+    } else {
+      return(default)      
+    }
+  }
+  
+  headers$From <- from
+  headers$To <- to
+  headers$Subject <- subject
+
+  ## Add Date header if not explicitly set. This fixes the annoyance,
+  ## that apparently Thunderbird does not sort mails correctly if they
+  ## do not have a Date header.
+  if (is.null(headers$Date))
+    headers$Date <- .rfc2822_date()
+  
+  transport <- get_value("transport", "smtp")
+  verbose <- get_value("verbose", FALSE)
+  if (transport == "smtp") {
+    server <- get_value("smtpServer", "localhost")
+    port <- get_value("smtpPort", 25)
+    
+    .smtp_submit_mail(server, port, headers, msg, verbose)
+  } else if (transport == "debug") {
+    .write_mail(headers, msg, stdout())
+  }
+}
