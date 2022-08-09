4a21f305264824273a48eb6e8b4d788ded570193
Are Edvardsen <biorakel@gmail.com>
2022-08-09 09:58:28 +0200

    simple test

--- tests/multiple_recipients.R
+++ tests/multiple_recipients.R
@@ -25,7 +25,7 @@ sendmail(from="from@example.org",
          subject="foo",
          msg="bar",
          control=list(transport="debug"))
-         
+
 sendmail(from="from@example.org",
          to="to1@example.org",
          cc=c("cc1@example.org", "cc2@example.org"),
@@ -44,5 +44,11 @@ sendmail(from="from@example.org",
          to="to1@example.org",
          bcc=c("bcc1@example.org", "bcc2@example.org"),
          subject="foo",
+         msg="bar",
+         control=list(transport="debug"))
+
+sendmail(from="from@example.org",
+         to="to1@example.org",
+         subject="K\u00f6ln",
          msg="bar",
          control=list(transport="debug"))
